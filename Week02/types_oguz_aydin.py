30fd3154ff2a78b1d9dccfa7cf352875a44c51cc
OguzAyd1n <121187961+OguzAyd1n@users.noreply.github.com>
2024-10-08 15:00:01 +0300

    Update types_oguz_aydin.py

--- Week02/types_oguz_aydin.py
+++ Week02/types_oguz_aydin.py
@@ -10,8 +10,4 @@ my_bool = True
 # Define a complex variable
 my_complex = 2 + 3j
 
-# Print the variables to verify their values and types
-print("my_int:", my_int, "Type:", type(my_int))
-print("my_float:", my_float, "Type:", type(my_float))
-print("my_bool:", my_bool, "Type:", type(my_bool))
-print("my_complex:", my_complex, "Type:", type(my_complex))
+
