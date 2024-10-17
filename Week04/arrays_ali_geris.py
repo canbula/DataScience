3baa5a20c2622dc7039901cc6da1f222ef20043f
Ali Geris <104502668+gerisali@users.noreply.github.com>
2024-10-17 22:50:16 +0300

    Update arrays_ali_geris.py

--- Week04/arrays_ali_geris.py
+++ Week04/arrays_ali_geris.py
@@ -8,23 +8,34 @@ def replace_center_with_minus_one(d, n, m):
     if n <= 0 or m <= 0:
         raise ValueError("n and m must be positive integers")
 
+    if m > n:
+        raise ValueError("m cannot be greater than n")
+
     # Rastgele d basamaklı sayılarla n x m boyutunda bir numpy array
     array = np.random.randint(0, 10 ** d, size=(n, m))
 
-    # Merkezi eleman(lar) -1
-    if n % 2 == 1 and m % 2 == 1:  # Hem satır hem sütun tek sayıysa
+    if n % 2 == 1 and m % 2 == 1:  # Tek boyutlar için ortadaki eleman
         center_row = n // 2
         center_col = m // 2
         array[center_row, center_col] = -1
-    else:  # Çift boyutlar için, ortadaki 4 eleman -1
+    elif n % 2 == 0 and m % 2 == 0:  # Çift boyutlar için ortadaki 4 eleman
         center_row1 = (n // 2) - 1
         center_row2 = n // 2
         center_col1 = (m // 2) - 1
         center_col2 = m // 2
         array[center_row1:center_row2 + 1, center_col1:center_col2 + 1] = -1
+    elif n % 2 == 1 and m % 2 == 0:  # Tek satır, çift sütun
+        center_row = n // 2
+        center_col1 = (m // 2) - 1
+        center_col2 = m // 2
+        array[center_row, center_col1:center_col2 + 1] = -1
+    elif n % 2 == 0 and m % 2 == 1:  # Çift satır, tek sütun
+        center_col = m // 2
+        center_row1 = (n // 2) - 1
+        center_row2 = n // 2
+        array[center_row1:center_row2 + 1, center_col] = -1
 
     return array
-
 
 # Test fonksiyonları
 def test_replace_center_with_minus_one():
