1c23c2502b5087468219d2c6c3686e561352fc13
Zülfükar Minaz <zlfkrmnz@gmail.com>
2024-10-17 12:09:59 +0300

    Create arrays_zulfukar_minaz.py

---
+++ Week04/arrays_zulfukar_minaz.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+
+
+def replace_center_with_minus_one(d: int, n: int, m: int) -> [[], []]:
+    if m > n or d <= 0 or m < 0 or n < 0:
+        raise ValueError
+    else:
+        arr = np.zeros((n, n), dtype=int)
+        if n == m:
+            arr[:, :] = -1
+            return arr
+        else:
+            for i in range(n):
+                for j in range(n):
+                    r = random.randint(1, 99)
+                    arr[i, j] = r
+            arr[((n - m) // 2):((n + m) // 2), ((n - m) // 2):((n + m) // 2)] = -1
+    return arr
