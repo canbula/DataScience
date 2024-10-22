a27f1ff357efb42f760091958ca0a235fa318d40
OguzAyd1n <121187961+OguzAyd1n@users.noreply.github.com>
2024-10-22 12:58:49 +0300

    Update arrays_oguz_aydin.py
    Rastgele sayılar 1 ile 10^d arasında değiştirilmiştir

--- Week04/arrays_oguz_aydin.py
+++ Week04/arrays_oguz_aydin.py
@@ -3,10 +3,11 @@ import numpy as np
 def replace_center_with_minus_one(d, n, m):
     if d <= 0 or m > n or n <= 0 or m <= 0:
         raise ValueError("Invalid input parameters.")
-    
-    arr = np.random.randint(0, 10**d, size=(n, n))
+
+    # Rastgele sayılar 1 ile 10^d arasında olmalı
+    arr = np.random.randint(1, 10**d, size=(n, n))
     center_start = (n - m) // 2
     center_end = center_start + m
     arr[center_start:center_end, center_start:center_end] = -1
-    
+
     return arr
